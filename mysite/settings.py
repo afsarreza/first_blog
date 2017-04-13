cbc8ddbba777af29c675b708f12af5ade5c4783d
afsarreza <afsar.reza1409@gmail.com>
2017-04-13 23:06:27 +0530

    added crud functionality and authority

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wik@=4+ncg)n$$v_m=p7yk_svu83=6#%!f4wfg@q&p-_9y8uf='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','..pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','..pythonanywhere.com']
 
 
 # Application definition
@@ -120,4 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+LOGIN_REDIRECT_URL = '/'
 
